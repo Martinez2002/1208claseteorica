247fc3a8a3616e549bb173b2db0f73540c53c3be
Martinez2002 <bryanthunder123@gmail.com>
2021-09-07 22:57:05 -0600

    se agregan los archivos

---
+++ 1208ClaseTeorica/Ejemplo1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1208ClaseTeorica
+{
+    class Ejemplo1
+    {
+        static void Main()
+        {
+            //diccionario.
+            //almacena pares de datos clave y valor.
+            //definir un diccionario.
+
+            Dictionary<string, string> EmployeList = new Dictionary<string, string>();
+
+            //Agregar elementos al diccionario
+            EmployeList.Add("Jose Flores", "Programador");
+            EmployeList.Add("Miranda Campos", "Generente");
+            EmployeList.Add("Carmen Valladares", "Arquietecta");
+
+            Dictionary<string, int> Students = new Dictionary<string, int>();
+            Students.Add("Jose Flores", 20);
+            Students.Add("Miranda Campos", 22);
+            Students.Add("Carmen Valladares", 24);
+
+            //leer el diccionario y mostrar los datos.
+            foreach (KeyValuePair<string, string> employee in EmployeList)
+            {
+                Console.WriteLine("Clave: {0}, Valor: {1}", employee.Key, employee.Value);
+            }
+
+            //ordenar los elementos del diccionario.
+
+            var dic = EmployeList.Keys.ToList();
+            dic.Sort();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("DICCIONARIO ORDENADO");
+
+            Console.WriteLine("\n");
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item}: {EmployeList[item]}");
+            }
+
+            Console.WriteLine("\n");
+            //contar los elementos del diccionario.
+            Console.WriteLine($"ELEMENTOS DE EL DICCIONARIO: {EmployeList.Count}");
+
+            Console.WriteLine();
+
+            //ejercicio 3 de la clase de programacion.
+            Dictionary<string, int> BryanMartinez = new Dictionary<string, int>();
+            BryanMartinez.Add("Bryan Martinez", 19);
+            BryanMartinez.Add("Ada Guevara", 21);
+            BryanMartinez.Add("Renes Martinez", 20);
+            BryanMartinez.Add("Melisa Morales", 25);
+
+            //leer el diccionario y mostrar los datos 
+            foreach (KeyValuePair<string, int> bra in BryanMartinez)
+            {
+                Console.WriteLine("Clave: {0}, Valor: {1}", bra.Key, bra.Value);
+            }
+
+            Console.WriteLine("\n");
+
+            //ordenar los elementos del diccionario
+            var familiaBryanMartinez = BryanMartinez.Keys.ToList();
+            familiaBryanMartinez.Sort();
+
+            Console.WriteLine("DICCIONARIO DE MANERA ORDENADA");
+            Console.WriteLine("\n");
+            foreach (var sec in familiaBryanMartinez)
+            {
+                Console.WriteLine("{0}: {1}", sec, BryanMartinez[sec]);
+            }
+
+            Console.WriteLine("\n");
+
+            //contar los elementos del diccionario
+            Console.WriteLine("ELEMENTOS DE EL DICCIONARIO: {0}", BryanMartinez.Count);
+            Console.ReadKey();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("By: Bryan Martinez...");
+        }
+    }
+}
---
+++ 1208ClaseTeorica/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1208ClaseTeorica
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //llamado al metodo loops
+            loops();
+
+            Console.ReadKey();
+        }
+
+        //bucles
+        public static void loops()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            //ciclo while
+            int i = 0;
+            Console.WriteLine("IMPRIME ELEMENTOS CON EL CICLO WHILE:");
+            while (i < numbers.Length)
+            {
+                Console.WriteLine(numbers[i]);
+                i++;
+            }
+
+            //ciclo do-while
+            Console.WriteLine("\n");
+            Console.WriteLine("IMPRIME ELEMENTOS CON EL CICLO DO-WHILE:");
+            int inc = 0;
+            do
+            {
+                Console.WriteLine(numbers[inc]);
+
+                inc++;
+            } while (inc < numbers.Length);
+
+            //ciclo for
+            Console.WriteLine("\n");
+            Console.WriteLine("IMPRIME ELEMENTOS CON EL CICLO FOR:");
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine("Posicion {0} : {1}", j, numbers[j]);
+            }
+
+            //ciclo foreach
+            Console.WriteLine("\n");
+            Console.WriteLine("IMPRIME ELEMENTOS CON EL CICLO FOREACH:");
+
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            //ejercicio realizado en clase
+            {
+                int[] arraynumeros = new int[10];
+
+                int sumaTotal = 0;
+                double promedio;
+
+                int n = 0;
+
+                while (n < arraynumeros.Length)
+                {
+                    Console.WriteLine("Ingrese el valor en la posicion {0}", n);
+                    arraynumeros[n] = Convert.ToInt32(Console.ReadLine());
+                    n++;
+                }
+
+                Console.WriteLine("\n");
+
+                Console.WriteLine("Mostrando datos del array ARRAYNUMEROS");
+
+                int m = 0;
+                Console.WriteLine("IMPRIME ELEMENTOS CON EL CICLO WHILE");
+                while (m < arraynumeros.Length)
+                {
+                    Console.WriteLine(arraynumeros[m]);
+                    m++;
+                }
+
+                Console.WriteLine();
+
+                //calculando promedio
+                for (int count = 0; count < 10; count++)
+                {
+                    sumaTotal = sumaTotal + arraynumeros[count];
+                }
+                promedio = sumaTotal / arraynumeros.Length;
+
+                Console.WriteLine("El promedio de los datos ingresados es: {0}", promedio);
+
+                Console.WriteLine("\n");
+
+                Console.WriteLine("By: Bryan Martinez...");
+
+            }
+        }
+    }
+}
